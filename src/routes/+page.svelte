2dc8bd89d95da961dd91ce25898df8643c631880
Andreas Wenzel <awenzel5@uoregon.edu>
2023-01-11 12:31:32 -0800

    mimimum reproducible repo

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+    import { enhance } from "$app/forms";
+</script>
+<a href=/method>Go To Method</a>
+<form method="POST" use:enhance>
+    <label for="name">Name="name"</label>
+    <input name="name"/>
+    <button type="submit">Submit</button>
+</form>
---
+++ src/routes/method/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+    import { enhance } from "$app/forms";
+</script>
+<a href="/">Go Home</a>
+<form method="POST" use:enhance>
+    <label for="name">Name="method"</label>
+    <input name="method"/>
+    <button type="submit">Submit</button>
+</form>
